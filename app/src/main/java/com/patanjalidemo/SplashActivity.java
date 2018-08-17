281a81730548e2f4c5eeffbe486382cbc4052284
hari-gaur <hari.gaur42@gmail.com>
2018-08-17 15:16:33 +0530

    project setup

--- app/src/main/java/com/patanjalidemo/SplashActivity.java
+++
@@ -1,7 +0,0 @@
-package com.patanjalidemo;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class SplashActivity extends AppCompatActivity {
-}
-
--- app/src/main/java/com/patanjalidemo/utils/Commons.java
+++
@@ -1,6 +0,0 @@
-package com.patanjalidemo.utils;
-
-public class Commons {
-
-    public static String APP_VERSION;
-}
