6323bd7cfca913744e59d228d3988c1c0d912457
Rishabh <rishabhjainwit5@gmail.com>
2018-08-17 14:23:20 +0530

    demo app initial code

---
+++ app/src/main/java/com/patanjalidemo/SplashActivity.java
@@ -0,0 +1,7 @@
+package com.patanjalidemo;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+}
+
---
+++ app/src/main/java/com/patanjalidemo/utils/Commons.java
@@ -0,0 +1,6 @@
+package com.patanjalidemo.utils;
+
+public class Commons {
+
+    public static String APP_VERSION;
+}
